bea9ec6edaa46eaed41f593921585570d593a540
David <draluy@yahoo.fr>
2019-04-21 00:58:25 +0200

    Create fatjar + disable jnativehook aggressive logging

--- src/main/java/fr/raluy/chocoratage/Main.java
+++ src/main/java/fr/raluy/chocoratage/Main.java
@@ -1,9 +1,21 @@
 package fr.raluy.chocoratage;
 
+import org.jnativehook.GlobalScreen;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class Main {
     public static void main(String[] args) {
+        configureJNativeHookLogger();
+
         KeyLogger keyLogger = new KeyLogger();
         SessionLocker sessionLocker = new SessionLocker();
         keyLogger.onChocoblastage(sessionLocker);
+    }
+
+    private static void configureJNativeHookLogger() {
+        Logger logger = Logger.getLogger(GlobalScreen.class.getPackage().getName());
+        logger.setLevel(Level.OFF);
     }
 }
